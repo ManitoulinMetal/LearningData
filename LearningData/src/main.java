5b3f7be656d6ad1d3dbbcf4e1be57cf9aa6a87e3
Werido <Werido@A>
2018-10-07 19:45:07 -0400

    Start of Data Types

---
+++ LearningData/src/main.java
@@ -0,0 +1,47 @@
+
+public class main {
+	
+	public static void main(String[] args) {
+		//Variables store data
+		// this "//" will make a comment
+		//Anything in a comment won't be compiled in the final ubild of the code
+		/* this notation can be used for
+		 * multi-lined comments
+		 * such as this
+		 * It is very important to add comments to you code as it makes it easier
+		 * for you to read and for others to read
+		 */
+		
+		//making variables
+		int integer;
+		double Double;
+		float Float;
+		char Character;
+		boolean Bool;
+		long Long;
+		short Short;
+		byte Byte;
+		String string;
+		
+		//defining variables
+		
+		//Below are whole value numbers, 8 bit, 16 bit, 32 bit, 64 bit
+		Byte = 10;
+		Short = 3;
+		integer = 90;
+		Long = 32000L;
+		
+		//Below are decimal or fraction value numbers, 32 bit, 64 bit
+		Float = 8.34f;
+		Double = 90.99d;
+		
+		//Below are textual values, single character, String is actually an object not a data type
+		Character = 'g';
+		string = "Hello World";
+		
+		//Below is boolean data, meaning it's true or false
+		Bool = true;
+		
+		
+	}
+}
